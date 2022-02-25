a73c8a6b80e3713349cb85c4bf7de9fa3560dd32
Nino171 <nino.heinzle@hotmail.com>
2022-02-25 12:45:12 +0100

    add analytics query

--- gimodules/CloudConnect/GIController.py
+++ gimodules/CloudConnect/GIController.py
@@ -2,6 +2,8 @@ import pwd
 import string
 from CloudConnect import CloudRequest as GINSCloud
 import logging
+import datetime as dt
+import requests
 #from gimodules.CloudConnect.CloudRequest import CloudRequest
 import httpx
 
@@ -44,18 +46,76 @@ class GIController():
     def filter_data(self, **kwargs):
         return
     
-    def get_variables(self, stream_id):
+    def get_variables(self, stream_id=None, stream_name=None):
         
         # write var names into lists
-        self.sensor_index, self.sensor_names, self.sensor_units, self.sensor_ids = self.conn_cloud.get_var_mapping(self.conn_cloud.request_map_res)
+        if stream_id != None:
+            response = self.conn_cloud.variable_mapping(stream_id)
+            response.raise_for_status()
+            self.sensor_index, self.sensor_names, self.sensor_units, self.sensor_ids = self.conn_cloud.get_var_mapping(response.json())
+            self.stream_id = stream_id
+        
+        #check if only one stream with this name exists else exception
         
         return self.sensor_ids,self.sensor_names
 
+    def get_timeseries_data(self, sensor_ids, resolution, start=None, end=None):
+        
+        start = start or dt.datetime(2021, 1, 1, tzinfo=dt.timezone.utc)
+        end = end or dt.datetime.now(dt.timezone.utc)
+        
+        url_postfix = '/__api__/gql'
+        sensor_indices = self._find_sensor_indices(sensor_ids)
+        indices_q_string = self._build_sensorid_querystring(sensor_indices)
+        
+        self.query = f"""
+            {{
+                analytics(
+                    from: {int(start.timestamp() * 1000)},
+                    to: {int(end.timestamp() * 1000)},
+                    resolution: {resolution},
+                    sid: "{self.stream_id}"
+                ) {{
+                    ts
+                    {indices_q_string}
+                }}
+            }}
+        """
+        response = requests.post(self.conn_cloud.url+url_postfix, json={'query':self.query}, headers={'Authorization': 'Bearer ' + self.conn_cloud.auth_token})
+        #response.raise_for_status()
+        content = response.json()
+        return content
     
+    def _find_sensor_indices(self, picked_sensor_ids):
+        if hasattr(self, 'sensor_ids'):
+            indices = []
+            for i in picked_sensor_ids:
+                indices.append(self.sensor_index[self.sensor_ids.index(i)])
+            return indices
+        raise ValueError('no such attribute in class.')
         
     
-    
+    def _build_sensorid_querystring(self, indices, aggregations=None):
+        #TODO
+        agg = """
+                avg
+        """
+        print(indices)
+        s = ""
+        for i in indices:
+            s = s+f"""
+                {i} {{
+                    {agg}
+                }}
+            """
+        print(s)
+        return s
 
+    def get_csv_export():
+        return
+    
+    def get_raw_data():
+        return
     class GIAuth():
         
         TOKEN = ''
