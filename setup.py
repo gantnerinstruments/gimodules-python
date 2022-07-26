d8280b71d3b99512c38faeb5aa786e3321c1a131
Nino171 <nino.heinzle@hotmail.com>
2022-07-26 10:23:25 +0200

    revert only push change - publish on release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3d'
+VERSION = '0.0.3e'
 DESCRIPTION = 'Python package to deliver a Gantner cloud interface'
 LONG_DESCRIPTION = ''
 
