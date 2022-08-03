b5210c7bc00272ea6a3b44e9de3c8e3d4f54776a
Nino171 <nino.heinzle@hotmail.com>
2022-08-03 11:51:05 +0100

    add mysql connector | enable splitting long term queries

--- gimodules/cloudconnect/utils.py
+++ gimodules/cloudconnect/utils.py
@@ -1,5 +1,6 @@
 import re
 import uuid
+from datetime import datetime
 
 def remove_hex_from_string(str):
    """Remove hex value from input string
@@ -13,3 +14,39 @@ def is_valid_uuid(val):
         return True
     except ValueError:
         return False
+    
+def _split_dates_gen(test_date1, test_date2, N):
+    
+    diff = (test_date2 - test_date1 ) / N
+    for idx in range(N):
+        
+        # using generator function to solve problem
+        # returns intermediate result
+        yield (test_date1 + diff * idx)
+    yield test_date2
+
+def split_dates(test_date1, test_date2, N):
+    """Split dates in N equally distanced date ranges in list
+    date obj e.g.:
+    test_date1 = datetime.datetime(1997, 1, 1)"""
+    temp = list(_split_dates_gen(test_date1, test_date2, N))
+
+    # using strftime to convert to userfriendly format
+    res = []
+    for sub in temp:
+        res.append(sub.strftime("%Y-%m-%d %H:%M:%S"))
+        
+    return res
+
+def get_dates_from_string(text: str) -> list[datetime]:
+    """Extract dates from a given string and return datetime objects in a list"""
+    matches = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', text)
+    for i, match in enumerate(matches):
+        matches[i] = datetime.strptime(match, '%Y-%m-%d %H:%M:%S')
+    return matches
+
+def replace_dates_in_string(text: str, dates: list) -> str:
+    new = text
+    for i, match in enumerate(re.finditer(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', text)):
+        new = "".join((new[:match.start()],dates[i],new[match.end():]))
+    return new
