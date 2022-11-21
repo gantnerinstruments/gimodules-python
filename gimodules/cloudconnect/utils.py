aa09f51c99247948a1a763e5f2f7d5d31e68e343
nhe8171 <nino.heinzle@hotmail.com>
2022-11-21 19:21:02 +0100

    typing for 3.7 compatibility

--- gimodules/cloudconnect/utils.py
+++ gimodules/cloudconnect/utils.py
@@ -1,6 +1,7 @@
 import re
 import uuid
 from datetime import datetime
+from typing import List
 
 def remove_hex_from_string(str):
    """Remove hex value from input string
@@ -38,14 +39,14 @@ def split_dates(test_date1, test_date2, N):
         
     return res
 
-def get_dates_from_string(text: str) -> list[datetime]:
+def get_dates_from_string(text: str) -> List[datetime]:
     """Extract dates from a given string and return datetime objects in a list"""
     matches = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', text)
     for i, match in enumerate(matches):
         matches[i] = datetime.strptime(match, '%Y-%m-%d %H:%M:%S')
     return matches
 
-def replace_dates_in_string(text: str, dates: list) -> str:
+def replace_dates_in_string(text: str, dates: List) -> str:
     new = text
     for i, match in enumerate(re.finditer(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', text)):
         new = "".join((new[:match.start()],dates[i],new[match.end():]))
