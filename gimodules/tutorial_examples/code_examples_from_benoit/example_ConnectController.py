57f9035f0f3480018e9583ad79fcf5f4f0dc8f78
Jovan ALAVANJA <aljo@fhv.at>
2024-01-30 15:23:28 +0100

    small refactoring of python dash tutorial - work in progress

--- gimodules/tutorial_examples/code_examples_from_benoit/example_ConnectController.py
+++ gimodules/tutorial_examples/code_examples_from_benoit/example_ConnectController.py
@@ -7,34 +7,31 @@ Gantner Instruments to Python tutorials
 Connect to GINS Controller
 """
 
-
-import initExample ## Add path to library (just for examples; you do not need this)
-
-import ginsapy.giutility.connect.PyQStationConnectWin as Qstation #module with communication functions to Gantner Q.Station under windows environment
-
-import numpy as np #module for scientific operations
-import pyqtgraph as pg # Scientific Graphics and GUI Library for Python. Use for fast display
+import initExample
+import numpy as np
+import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
+import ginsapy.giutility.connect.PyQStationConnectWin as Qstation
 
+#### Input parameters
+# Controller IP address
+controller_IP = "192.168.178.43"  
 
+# Channel number to be plotted. You can enter several indexes [1,2,3,4]. Index 0 is timestamp
+channel_nb = [8]  
 
-#****************************************************
-# Input parameters
-#*****************************************************
+# Size of the plotting window
+window_size = 4096  
 
-controller_IP="192.168.178.43"#Controller IP
-channel_nb=[8]#channel number to be ploted you can enter several indexes[1,2,3,4]. Index 0 is timestamp
-window_size=4096# Size of the ploting window
-buffer_index=0#Enter the index of the buffer
-#****************************************************
+# Enter the index of the buffer
+buffer_index = 0  
 
-
-#Initialisation of a buffer connection
-conn=Qstation.ConnectGIns()
-conn.bufferindex=int(buffer_index)
+# Initialization of a buffer connection
+conn = Qstation.ConnectGIns()
+conn.bufferindex = int(buffer_index)
 conn.init_connection(str(controller_IP))
 
-#Return some information of the controller
+# Return some information of the controller
 conn.read_controller_name()
 conn.read_serial_number()
 conn.read_sample_rate()
@@ -42,55 +39,48 @@ conn.read_channel_names()
 conn.read_channel_count()
 print(conn.read_index_name(0))
 
+# Call function to store Controller buffer into the variable buffer
+buffer = conn.yield_buffer()
 
-#Call function to store Controller buffer into the variable buffer
-buffer=conn.yield_buffer()
-
-
-#function to initiate a pyqtgraph plot
-def init_curve(graph,init_list):
-    curve=[]
+# Function to initiate a pyqtgraph plot
+def init_curve(graph, init_list):
+    curve = []
     for i in range(len(init_list)):
         c = graph.plot()
         curve.append(c)
-    return(curve)
+    return curve
 
-
-#*************
-#Visualisation of the measurements
-#*************
-
-
-
-signal_plot=np.zeros((window_size,len(channel_nb)))#output matrix initialisation
-
+# Visualisation of the measurements
+signal_plot = np.zeros((window_size, len(channel_nb)))  # Output matrix initialization
 
 win = pg.GraphicsWindow(title="Basic plotting example")
-win.resize(1000,600)
+win.resize(1000, 600)
 win.setWindowTitle('Time Signal')
 
 plot_1 = win.addPlot(title="Title: Time plot")
-curves=init_curve(plot_1,channel_nb)
+curves = init_curve(plot_1, channel_nb)
 
 win.show()
 
-#Function to update graph
+# Function to update the graph in real time with the new data
 def update():
-    global curves,channel_nb,buffer,signal_plot,window_size,readbuffer
-    readbuffer=next(buffer)          #get next buffer frames
-    disp = readbuffer[:,channel_nb]  #get channel columns out of readbuffer matrix
-    dim=len(disp)                    #number of rows/dataframes
-    signal_plot=np.vstack((signal_plot[dim:window_size,:],disp))
+    global curves, channel_nb, buffer, signal_plot, window_size, readbuffer
+     # Get next buffer frames
+    readbuffer = next(buffer)
 
-    for i in range(0,len(channel_nb)):
-        curves[i].setData(y=signal_plot[:,i])
+    # Get channel columns out of readbuffer matrix 
+    disp = readbuffer[:, channel_nb]  
 
+    # Number of rows/dataframes
+    dim = len(disp)  
+    signal_plot = np.vstack((signal_plot[dim:window_size, :], disp))
 
+    for i in range(0, len(channel_nb)):
+        curves[i].setData(y=signal_plot[:, i])
 
-timer=QtCore.QTimer()
+timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(100)#refreshms
+
+# Refresh interval in milliseconds
+timer.start(100)  
 pg.QtGui.QGuiApplication.exec_()
-
-
-
--- gimodules/tutorial_examples/how_to_interact_with_Q_station.py
+++ gimodules/tutorial_examples/how_to_interact_with_Q_station.py
@@ -3,12 +3,11 @@ File: how_to_interact_with_Q_station.py
 Author: aljo
 
 Created on: 18. January 2024
-Last changed: 23. January 2024
+Last changed: 30. January 2024
 
 Description:
 Tutorial Projekt made for Clients of Gantert Instruments 
 '''
-
 import os
 import dash
 from dash import dcc, html, dash_table
@@ -17,7 +16,6 @@ import dash_bootstrap_components as dbc
 from datetime import timedelta
 import datetime as dt 
 import plotly.graph_objs as go
-
 
 # local module
 from gimodules.cloudconnect.cloud_request import CloudRequest
@@ -32,9 +30,7 @@ script_folder = 'gimodules/tutorial_examples/code_examples_from_benoit'
 filtered_scripts = [script for script in os.listdir(script_folder) if script.startswith("example_")]
 
 
-
 ########################## Helper Functions
-
 ## cloud functions
 def get_stream_metadata(url, user, pw,): 
     cloud = CloudRequest()
@@ -47,16 +43,7 @@ def get_stream_metadata(url, user, pw,):
     return streams_and_variables, cloud.login_token
 
 
-
-def get_data_from_cloud_datastream(token, stream, variable):
-    cloud = CloudRequest()
-    cloud.login_token = token
-    data = cloud.get_data(stream, variable)
-    return data
-
 ## local functions
-
-
 def connect_to_q_station(controller_IP, dll_path):
     #Initialisation of a buffer connection
     conn=Qstation.ConnectGIns(dll_path)
@@ -101,7 +88,165 @@ def create_q_station_table(buffer_data, c_channel_names):
     else: 
         channel_data = [{'Channel index': i, 'Channel name': c_channel_names[i], 'Channel value': buffer_data[0][i]} for i in c_channel_names.keys()] 
     return channel_data
-    
+
+
+## code snippets 
+# example 1 - how to use python GraphQL API 
+code_snippet_graphQL = """
+from datetime import timedelta
+import datetime as dt 
+
+# local module
+from gimodules.cloudconnect.cloud_request import CloudRequest
+
+# Basic GraphQL example
+# Variables
+url = 'https://my-gi-cloud.com'
+user = 'username'
+pw = 'password'
+
+
+# Login to your cloud account
+cloud = CloudRequest()
+cloud.login(url, user, pw)
+
+# Get gi data streams
+print(cloud.streams)
+
+# Get a stream by name
+print(cloud.streams['MID4'])
+
+# Look for variables in a stream
+cloud.find_var('MID4')
+
+# Get data from a stream
+stream_id = cloud.streams[datastream].id
+var_index = cloud.stream_variabels['{' + datastream + '}__{' + variable + '}'].index
+
+# use the last timestamp as end date and the last timestamp minus one day as start date
+end_date = dt.datetime.utcfromtimestamp((float(cloud.streams[datastream].last_ts)/1000)).strftime('%Y-%m-%d %H:%M:%S')
+start_date = (dt.datetime.utcfromtimestamp(float(cloud.streams[datastream].last_ts) / 1000) - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+
+# get data from cloud
+df = cloud.get_var_data(stream_id, [var_index], start_date, end_date, 'MINUTE').set_index('Time')
+"""
+
+## example 2 - how to connect to Q.station via LAN
+code_snippet_q_station = """
+import gimodules.py_q_station_connect_cloud.py_q_station_connect_cloud as Qstation 
+
+# Connect to Q.station
+conn = Qstation.ConnectGIns(dll_path)
+conn.bufferindex = int(0)
+conn.init_connection(str(controller_IP))
+
+# Read controller information
+c_name = conn.read_controller_name()
+c_serial_number = conn.read_serial_number()
+c_sample_rate = conn.read_sample_rate()
+c_channel_names = conn.read_channel_names()
+c_number_of_channels = conn.read_channel_count()
+
+# Read data from buffer
+buffer = conn.yield_buffer()
+buffer_result = []
+stop_counter = 0
+
+# Reading the Buffer works really fast, so we need to stop the loop after a while
+for i in range(1, 9999999): 
+    res = next(buffer)
+    if len(res) != 0:
+        buffer_result.append(res)
+        # Data found, break the loop
+        break
+    stop_counter += 1
+    if stop_counter > 1000:
+        # No data found, stop the loop
+        break      
+"""
+
+
+## code snippet 3 - how to create advanced charts
+code_snippet_advanced_example_chart_1 = """
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set Seaborn style
+sns.set_style("whitegrid")
+
+# Adjust data by dividing by 400
+adjusted_data = df['PRdc']
+weights = adjusted_data.values
+
+# Create initial plot with a larger figure size
+fig, ax1 = plt.subplots(figsize=(15, 8))
+
+# Histogram with sum of values per bin on the primary Y-axis (ax1)
+ax1.hist(adjusted_data, weights=weights, bins=16, edgecolor='black', alpha=0.7, label='Sum per bin')
+ax1.set_xlabel('PRdc')
+ax1.set_ylabel('count', color='blue')
+ax1.tick_params(axis='y', labelcolor='blue')
+ax1.legend(loc='upper left')
+#ax1.set_ylim([0, 4000])  # Setting y1-axis scaling from 0 to 10000
+
+
+# Create secondary Y-axis (ax2)
+ax2 = ax1.twinx()
+# Cumulative distribution on the secondary Y-axis (ax2)
+ax2.hist(adjusted_data, weights=weights, bins=16, edgecolor='black', alpha=0.7, cumulative=True, histtype='step', label='Cumulative sum', color='red')
+ax2.set_ylabel('Cumulative Sum nPmpp-MID4-OTF21', color='red')
+ax2.tick_params(axis='y', labelcolor='red')
+ax2.legend(loc='upper right')
+
+# Title for the plot
+plt.title('Histogram and Cumulative Distribution with Adjusted Data')
+
+# Display the plot
+plt.show()
+"""
+
+## code snippet 4 - how to create advanced charts
+code_snippet_advanced_example_chart_2 = """
+import plotly.express as px
+
+if 'Matrix' in choose_chart.value:
+    fig = px.scatter_matrix(df,
+        dimensions=[
+            'Pdc', 'Idc', 'Vdc', 'nIdc_1', 'nIdc_2', 'nIdc_3',
+       'nIdc_4', 'nIdc_5', 'nIdc_6'
+        ],
+        color=color_w.value,
+        title="MIDx matrix plot",
+        color_continuous_scale='Viridis',
+        range_color=[0, 36],
+        )
+
+    fig.update_layout(
+        dragmode='select',
+        width=1200,
+        height=1200,
+        hovermode='closest',
+    )
+    fig.update_traces(diagonal_visible=False)
+    fig.show()
+
+"""
+## code snippet 5 - how to use dash
+code_snippet_basic_dash_script = """
+# Run this app with `python app.py` and
+
+from dash import Dash, dcc, html
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Hello World")
+])
+
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0", port=8050) # You need to use the host 0.0.0.0 and the port 8050
+"""
+
 
 ########################## Basic Components 
 header = dbc.Row([
@@ -110,7 +255,7 @@ header = dbc.Row([
                 ]),
                 dbc.Col([
                     html.H2("Hands on Python Tutorial"),
-                    html.Span("Learn how to use Python to connect to your Q.Station and to the GI.Cloud."),
+                    html.Span("Learn how to use Python to connect to your Q.station and to the GI.Cloud."),
                     html.Br(),
                     html.Span("For more information visit the "), 
                     html.A("Gantner Docs", href=DOCS_URL, target="_blank", style={"color" : "#0053a5"})
@@ -123,8 +268,8 @@ body = dbc.Row([
         #dcc.Tabs(id="tabs-styled-with-inline", value='page_dash_cloud_example', vertical=True, children=[
         dcc.Tabs(id="tabs-styled-with-inline", value='page_connect_local_to_q_station', vertical=True, children=[
             dcc.Tab(label='How to use Cloud Analytics', value='page_dash_cloud_example', style={"border-top" : "0px solid"}),
-            dcc.Tab(label='Connect to Q.Station via Cloud', value='page_connect_cloud_to_q_station'),
-            dcc.Tab(label='Connect to Q.Station via LAN', value='page_connect_local_to_q_station'),
+            dcc.Tab(label='Connect to Q.station via Cloud', value='page_connect_cloud_to_q_station'),
+            dcc.Tab(label='Connect to Q.station via LAN', value='page_connect_local_to_q_station'),
             dcc.Tab(label='How to create advanced Charts', value='page_advanced_code_preview'),
             dcc.Tab(label='More Code examples', value='page_code_preview')
         ])
@@ -179,21 +324,6 @@ def render_content(tab):
 
 ########################## Navigation Tabs Content
 ########################## page_dash_cloud_example
-basic_dash_script = """
-# Run this app with `python app.py` and
-
-from dash import Dash, dcc, html
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.H1("Hello World")
-])
-
-if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050) # You need to use the host 0.0.0.0 and the port 8050
-"""
-
 page_dash_cloud_example =  dbc.Row([
     html.H3("How to use Cloud Analytics"),
     html.P("The following steps will guide you through the process of creating a Cloud Analytics app."),
@@ -203,7 +333,7 @@ page_dash_cloud_example =  dbc.Row([
     html.Img(src='assets/add-script.png'),
     html.P("3. Copy the following code snippet into your script."),
     html.Div([
-        dcc.Markdown(children=f"""```python{basic_dash_script}```""", ),
+        dcc.Markdown(children=f"""```python{code_snippet_basic_dash_script}```""", ),
     ], className="border border-primary rounded p-3"), 
     html.P("4. Open a new terminal."),
     html.Img(src='assets/run-terminal.png'),
@@ -278,7 +408,7 @@ def update_datastream_variables(datastream, data):
 
 
 cloud_stream_data_component = dbc.Row([
-    html.H3("Select your Q.Station datastream"),
+    html.H3("Select your Q.station datastream"),
     dbc.Label("Streamname", width=1),
     dbc.Col([
         dcc.Dropdown(id="cloud-datastream", placeholder="no datastream available"),
@@ -359,7 +489,10 @@ page_connect_dash_to_cloud = dbc.Row([
         # components
         cloud_login_component, 
         cloud_stream_data_component,
-        html.Div(id="cloud-chart-container", children=[])
+        html.Div(id="cloud-chart-container", children=[]),
+        html.Div([
+            dcc.Markdown(children=f"""```python{code_snippet_graphQL}```""", ),
+        ], className="border border-primary rounded p-3") 
     ])
 
 
@@ -367,8 +500,8 @@ page_connect_dash_to_cloud = dbc.Row([
 ########################## page_connect_local_to_q_station
 page_connect_local_to_q_station = dbc.Row([
         dbc.Row([
-            html.H3("Connect to Q.Station via LAN"),
-            html.P("For connection with the Q.Station via LAN, the Q.Station needs to be connected to the same network as your computer. You also need the dll File from GI to communicate with your Q.station"),
+            html.H3("Connect to Q.station via LAN"),
+            html.P("For connection with the Q.station via LAN, the Q.station needs to be connected to the same network as your computer. You also need the dll File from GI to communicate with your Q.station"),
             html.P("You can download the dll file from the GI support website."),
             dbc.Row([
                     dbc.Label("Enter the full path to utility dll."),
@@ -379,32 +512,23 @@ page_connect_local_to_q_station = dbc.Row([
         ]),
 
         dbc.Row([
-            html.H3("Enter Q Station Parameters"),
-            html.P("Enter the IP address of the Q Station controller.", className="m-0 p-0"),
+            html.H3("Enter Q station Parameters"),
+            html.P("Enter the IP address of the Q station controller.", className="m-0 p-0"),
             dbc.Form([
                 dbc.Row([
-                    dbc.Label("Q Station Controller IP", width=8),
+                    dbc.Label("Q station Controller IP", width=8),
                     dbc.Col([
                         dcc.Input(id="q-station-ip", type="text", placeholder="192.168.178.80", value="192.168.178.80"),
                     ], width=10)
                 ], className="mb-3" ),
-                # dbc.Row([
-                #     dbc.Label("Q Station Channel Number", width=8),
-                #     dbc.Col([
-                #               dcc.Input(id="q_station_channel_nb", type="text", placeholder="8"),
-                #     ], width=10)
-                # ], className="mb-3"),
-                # dbc.Row([
-                #     dbc.Label("Q Station Buffer Index", width=8),
-                #     dbc.Col([
-                #         dcc.Input(id="q_station_buffer_index", type="text", placeholder="0"),
-                #     ], width=10)
-                # ], className="mb-3"),
-                dbc.Button('Connect to Q.Station', id='connect-to-q-station', n_clicks=0, style={"background-color" : "#0053a5"}, className="mt-3"),
+                dbc.Button('Connect to Q.station', id='connect-to-q-station', n_clicks=0, style={"background-color" : "#0053a5"}, className="mb-3 mt-3"),
             ])
         ]),
 
         html.Div(id="q-station-connection", children=[]),
+        html.Div([
+            dcc.Markdown(children=f"""```python{code_snippet_q_station}```""", ),
+        ], className="border border-primary rounded p-3"), 
         ])
 
 # component callback
@@ -423,11 +547,11 @@ def connect_to_q_station_callback(n_clicks, ip, dll_path):
         if dll_path is None:
             return [html.P("No dll path provided. Please provide a dll path.")]
         
-        # connect to Q.Station
+        # connect to Q.station
         c_name, c_serial_number, c_sample_rate, c_channel_names, c_number_of_channels = connect_to_q_station(ip, os.path.abspath(dll_path))
 
         result = [
-            html.P(f"Connected to Q.Station {c_name}."),
+            html.P(f"Connected to Q.station {c_name}."),
             html.P(f"Serial number: {c_serial_number}"),
             html.P(f"Sample rate: {c_sample_rate}"),
             html.P(f"Number of channels: {c_number_of_channels}")
@@ -458,7 +582,7 @@ def connect_to_q_station_callback(n_clicks, ip, dll_path):
 
 def update_chart_callback(n_intervals, ip, dll_path):
 
-    # connect to Q.Station
+    # connect to Q.station
     buffer_data, c_channel_names = read_q_station_stream_buffer(ip, os.path.abspath(dll_path))
     channel_data = create_q_station_table(buffer_data, c_channel_names)
     return channel_data
@@ -468,83 +592,17 @@ def update_chart_callback(n_intervals, ip, dll_path):
 
 
 ########################## page_advanced_code_preview
-example_chart_1 = """
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Set Seaborn style
-sns.set_style("whitegrid")
-
-# Adjust data by dividing by 400
-adjusted_data = df['PRdc']
-weights = adjusted_data.values
-
-# Create initial plot with a larger figure size
-fig, ax1 = plt.subplots(figsize=(15, 8))
-
-# Histogram with sum of values per bin on the primary Y-axis (ax1)
-ax1.hist(adjusted_data, weights=weights, bins=16, edgecolor='black', alpha=0.7, label='Sum per bin')
-ax1.set_xlabel('PRdc')
-ax1.set_ylabel('count', color='blue')
-ax1.tick_params(axis='y', labelcolor='blue')
-ax1.legend(loc='upper left')
-#ax1.set_ylim([0, 4000])  # Setting y1-axis scaling from 0 to 10000
-
-
-# Create secondary Y-axis (ax2)
-ax2 = ax1.twinx()
-# Cumulative distribution on the secondary Y-axis (ax2)
-ax2.hist(adjusted_data, weights=weights, bins=16, edgecolor='black', alpha=0.7, cumulative=True, histtype='step', label='Cumulative sum', color='red')
-ax2.set_ylabel('Cumulative Sum nPmpp-MID4-OTF21', color='red')
-ax2.tick_params(axis='y', labelcolor='red')
-ax2.legend(loc='upper right')
-
-# Title for the plot
-plt.title('Histogram and Cumulative Distribution with Adjusted Data')
-
-# Display the plot
-plt.show()
-
-"""
-
-example_chart_2 = """
-
-import plotly.express as px
-
-if 'Matrix' in choose_chart.value:
-    fig = px.scatter_matrix(df,
-        dimensions=[
-            'Pdc', 'Idc', 'Vdc', 'nIdc_1', 'nIdc_2', 'nIdc_3',
-       'nIdc_4', 'nIdc_5', 'nIdc_6'
-        ],
-        color=color_w.value,
-        title="MIDx matrix plot",
-        color_continuous_scale='Viridis',
-        range_color=[0, 36],
-        )
-
-    fig.update_layout(
-        dragmode='select',
-        width=1200,
-        height=1200,
-        hovermode='closest',
-    )
-    fig.update_traces(diagonal_visible=False)
-    fig.show()
-
-"""
-
 
 page_advanced_code_preview = dbc.Row([
     html.H3("Advanced Python Code Snippet Example"),
 
     html.Div([
-        dcc.Markdown(children=f"""```python{example_chart_1}```""", ),
+        dcc.Markdown(children=f"""```python{code_snippet_advanced_example_chart_1}```""", ),
     ], className="border border-primary rounded p-3"), 
     html.Img(src='assets/example_chart_1.png'),
     
     html.Div([
-        dcc.Markdown(children=f"""```python{example_chart_2}```""", ),
+        dcc.Markdown(children=f"""```python{code_snippet_advanced_example_chart_2}```""", ),
     ], className="border border-primary rounded p-3"), 
     html.Img(src='assets/example_chart_2.png'),
 ])
@@ -568,13 +626,11 @@ page_code_preview = dbc.Col([
         ),
         html.Div([
             dcc.Markdown(id='output-markdown'),
-        ], className="border border-primary rounded p-3")#, 
-        #dbc.Button('Run script', n_clicks=0, style={"background-color" : "#0053a5"}, className="mt-3"),
+        ], className="border border-primary rounded p-3")
     ])
 
 
 # component callback
-
 @app.callback(
     Output('output-markdown', 'children'),
     [Input('script-dropdown', 'value')]
@@ -596,5 +652,5 @@ def update_output(selected_script):
 
 ###########################Server 
 def run():
-    app.run_server(debug=False)
+    app.run_server(debug=True)
     #app.run_server(host="0.0.0.0", port=8050) # You need to use the host 0.0.0.0 and the port 8050
