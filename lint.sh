f66443e18b0d378745092bb28967aa63839ec95f
n <nino.heinzle@hotmail.com>
2024-10-10 14:30:14 +0200

    Use line length = 100 instead 79
    Signed-off-by: n <nino.heinzle@hotmail.com>

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running black (code formatting)..."
-black --config ./pyproject.toml --safe --line-length 100 "$1"
+black --safe --line-length 100 "$1"
 echo "Running flake8 (pep8)..."
 # ignore E203 and W503 conflicts of black and flake8
 flake8 --max-line-length=100 --ignore=E203,W503 "$1"
